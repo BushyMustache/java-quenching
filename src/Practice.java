cd4b4648fcfbef5c1b25514e77cb905189843529
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-09-22 17:41:18 -0700

    Completed levelCount method and its tests

--- src/Practice.java
+++ src/Practice.java
@@ -125,7 +125,13 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        
+        int leftLevels = levelCount(root.left);
+        int rightLevels = levelCount(root.right);
+
+        return 1 + Math.max(leftLevels, rightLevels);
     }
 
 
